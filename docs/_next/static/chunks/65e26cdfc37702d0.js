592c678ee4274be2656afa05185e4bf6d36385a0
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-01-04 10:12:21 +0700

    update packages

--- docs/_next/static/chunks/65e26cdfc37702d0.js
+++ docs/_next/static/chunks/65e26cdfc37702d0.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/1dc531fe095acdba.js",
+  "static/chunks/80a965ad83983caf.js",
   "static/chunks/95e7e53f489a231d.js",
   "static/chunks/f5fb0312e90afb59.css",
-  "static/chunks/turbopack-96039f4d2d9cd05b.js"
+  "static/chunks/turbopack-0dd1b6c87e327cf9.js"
 ])
