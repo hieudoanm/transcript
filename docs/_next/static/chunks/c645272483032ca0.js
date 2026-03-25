bf6141112b13dde4040e12253f0fd7bad7a19585
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-25 21:24:13 +0700

    Use HeadTemplate

--- docs/_next/static/chunks/c645272483032ca0.js
+++ docs/_next/static/chunks/c645272483032ca0.js
@@ -1,8 +1,9 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/082595e305de9b07.js",
+  "static/chunks/d0813d6597423fb9.js",
+  "static/chunks/26b4d31bbceaba05.js",
   "static/chunks/db23ce64154faceb.js",
   "static/chunks/d7d17908e2d59ebc.js",
   "static/chunks/24b593d92d79fdc3.css",
   "static/chunks/d782668ea84e48c1.css",
-  "static/chunks/turbopack-ceffb63130c7522d.js"
+  "static/chunks/turbopack-62dc3f23c5b26447.js"
 ])
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '@transcript/styles/globals.css';
+import { HeadTemplate } from '../templates/HeadTemplate';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Head from 'next/head';
 import { FC } from 'react';
 
 const geistSans = Geist({
@@ -18,13 +18,7 @@ const geistMono = Geist_Mono({
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>Transcript</title>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <HeadTemplate basic={{ title: 'Transcript' }} />
       <QueryClientProvider client={new QueryClient()}>
         <div className={`${geistSans.className} ${geistMono.className}`}>
           <Component {...pageProps} />
