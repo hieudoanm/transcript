c8624a026ae93b1c333c049b83f04f2e4584dd40
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-05-04 12:11:37 +0700

    remove cli

---
+++ docs/_next/static/K8zvTUMecRH86AzHWjA0N/_buildManifest.js
@@ -0,0 +1,38 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0o0vkdnj-158l.js"
+  ],
+  "/404": [
+    "static/chunks/0ws-s.wglxrnv.js"
+  ],
+  "/500": [
+    "static/chunks/09dn.qy57p5~8.js"
+  ],
+  "/_error": [
+    "static/chunks/00punlxjjxhvg.js"
+  ],
+  "/app": [
+    "static/chunks/0ifn.6fuk3y-p.js"
+  ],
+  "/downloads": [
+    "static/chunks/0uwxqlq-ox2t8.js"
+  ],
+  "/version": [
+    "static/chunks/0e_gyulbnvte2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/500",
+    "/_app",
+    "/_error",
+    "/app",
+    "/downloads",
+    "/version"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
